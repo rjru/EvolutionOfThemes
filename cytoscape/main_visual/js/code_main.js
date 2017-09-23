cf52a1172a056f301e811a70df2f566eb57d8526
R. R. Urquiaga <rjruos@gmail.com>
2017-09-23 17:02:09 -0500

    finish result of time series

--- cytoscape/main_visual/js/code_main.js
+++ cytoscape/main_visual/js/code_main.js
@@ -1,62 +1,67 @@
+   $( document ).ready(function() {
+        console.log( "document loaded" );
 
-        // cyto2.json
-$.getJSON("data/syntetic_control_300.json", function (data) {
-    var cy = cytoscape({
-	          container: document.getElementById('cy'),
-              elements: data,
-              // so we can see the ids
-              style: 
-              [
-                {
-                  selector: 'node',
-                  style:{
-                        //'label': 'data(label)',
-                        //'width': '0.5px',
-                        //'height': '0.5px',
-                        'width': '0.015em',
-                        'height': '0.015em',
-                        'color': 'yellow',
-                        'background-fit': 'contain',
-                        'background-clip': 'none',
-                        'border-style': 'solid',
-                        'border-width': '0.0002em',
-                        'border-color': 'white',
-                        //'font-size' : '0.5px',
-                        'font-size' : '0.02em',
-                        'background-color': 'data(class)'
-                  		}
-                },
-                {
-                  selector: 'edge',
-                  style: {
-                        'text-background-color': 'yellow',
-                        'text-background-opacity': 0.4,
-                        //'width': '0.05em',
-                        'width': '0.0015em',
-                        'control-point-step-size': '0.3em'
-                         }
-                }
-              ],
 
-              layout: {
-	                  name: 'preset',
-	                  positions: undefined, // map of (node id) => (position obj); or function(node){ return somPos; }
-	                  zoom: undefined, // the zoom level to set (prob want fit = false if set)
-	                  pan: undefined, // the pan level to set (prob want fit = false if set)
-	                  fit: true, // whether to fit to viewport
-	                  padding: 20, // padding on fit
-	                  animate: true, // whether to transition the node positions
-	                  animationDuration: 500, // duration of animation in ms if enabled
-	                  animationEasing: undefined, // easing of animation if enabled
-	                  animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-	                  ready: undefined, // callback on layoutready
-	                  stop: undefined, // callback on layoutstop
-	                  transform: function (node, position ){ return position; }, // transform a given node position. Useful for changing flow direction in discrete layouts 
-	              	  }
-                  //layout:{defaults}
-            });
+$("#result_from").change(function () {
+	  var namefile = $("#result_from :selected").attr('value');
+	  console.log(namefile)
+	  $.ajax({dataType:"json", url: "https://raw.githubusercontent.com/rjru/EvolutionOfThemes/master/time_series/"+namefile, success: function(result){
+			  var cy = cytoscape({
+			  container: document.getElementById('cy'),
+			  elements: result,
+			  // so we can see the ids
+			  style: 
+			  [
+			    {
+			      selector: 'node',
+			      style:{
+			            //'label': 'data(label)',
+			            //'width': '0.5px',
+			            //'height': '0.5px',
+			            'width': '0.015em',
+			            'height': '0.015em',
+			            'color': 'yellow',
+			            'background-fit': 'contain',
+			            'background-clip': 'none',
+			            'border-style': 'solid',
+			            'border-width': '0.0002em',
+			            'border-color': 'white',
+			            //'font-size' : '0.5px',
+			            'font-size' : '0.02em',
+			            'background-color': 'data(class)'
+			      		}
+			    },
+			    {
+			      selector: 'edge',
+			      style: {
+			            'text-background-color': 'yellow',
+			            'text-background-opacity': 0.4,
+			            //'width': '0.05em',
+			            'width': '0.0015em',
+			            'control-point-step-size': '0.3em'
+			             }
+			    }
+			  ],
 
-            //cy.add(data)
+			  layout: {
+			          name: 'preset',
+			          positions: undefined, // map of (node id) => (position obj); or function(node){ return somPos; }
+			          zoom: undefined, // the zoom level to set (prob want fit = false if set)
+			          pan: undefined, // the pan level to set (prob want fit = false if set)
+			          fit: true, // whether to fit to viewport
+			          padding: 20, // padding on fit
+			          animate: true, // whether to transition the node positions
+			          animationDuration: 500, // duration of animation in ms if enabled
+			          animationEasing: undefined, // easing of animation if enabled
+			          animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+			          ready: undefined, // callback on layoutready
+			          stop: undefined, // callback on layoutstop
+			          transform: function (node, position ){ return position; }, // transform a given node position. Useful for changing flow direction in discrete layouts 
+			      	  }
+			      //layout:{defaults}
+			}); // fin citoscape
+
+			//cy.add(data)
 			//console.log(data.edges[0]["data"]["length"])
 
 			var defaults = {
@@ -240,88 +245,92 @@ $.getJSON("data/syntetic_control_300.json", function (data) {
 
 			  graph.render();
 			  // */ 
+			}); // fin click
+
+			$('#config-toggle').on('click', function(){
+			  $('body').toggleClass('config-closed');
+			  cy.resize();
 			});
+			    
+	},
+	error:function (xhr, ajaxOptions, thrownError){
+	    if(xhr.status==404) {
+	    	  console.log("horror!")
+			  var cy = cytoscape({
+			  container: document.getElementById('cy'),
+			  elements: undefined});
+	    }
+	}
+	}); // fin ajax
 
-    		$('#config-toggle').on('click', function(){
-		      $('body').toggleClass('config-closed');
-		      cy.resize();
-		    });
-		
-			$("#result_from").change(function () {
-				var path = "C:/Users/rbrto-pc/Google Drive/citation_lda/src/time_series"
-				var namefile = $("#result_from :selected").attr('value');
-				$.getJSON("data/"+namefile, function (data) {
-					console.log(data)
-				});
-
-			});
-
-		      /*
-		      $button.on('click', function(){
-		      	$.getJSON("data/syntetic_control_600.json", function (data) {
-    				var cy = cytoscape({
-	          container: document.getElementById('cy'),
-              elements: data,
-              // so we can see the ids
-              style: 
-              [
-                {
-                  selector: 'node',
-                  style:{
-                        //'label': 'data(label)',
-                        //'width': '0.5px',
-                        //'height': '0.5px',
-                        'width': '0.015em',
-                        'height': '0.015em',
-                        'color': 'yellow',
-                        'background-fit': 'contain',
-                        'background-clip': 'none',
-                        'border-style': 'solid',
-                        'border-width': '0.0002em',
-                        'border-color': 'white',
-                        //'font-size' : '0.5px',
-                        'font-size' : '0.02em',
-                        'background-color': 'data(class)'
-                  		}
-                },
-                {
-                  selector: 'edge',
-                  style: {
-                        'text-background-color': 'yellow',
-                        'text-background-opacity': 0.4,
-                        //'width': '0.05em',
-                        'width': '0.0015em',
-                        'control-point-step-size': '0.3em'
-                         }
-                }
-              ],
-
-              layout: {
-	                  name: 'preset',
-	                  positions: undefined, // map of (node id) => (position obj); or function(node){ return somPos; }
-	                  zoom: undefined, // the zoom level to set (prob want fit = false if set)
-	                  pan: undefined, // the pan level to set (prob want fit = false if set)
-	                  fit: true, // whether to fit to viewport
-	                  padding: 20, // padding on fit
-	                  animate: true, // whether to transition the node positions
-	                  animationDuration: 500, // duration of animation in ms if enabled
-	                  animationEasing: undefined, // easing of animation if enabled
-	                  animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-	                  ready: undefined, // callback on layoutready
-	                  stop: undefined, // callback on layoutstop
-	                  transform: function (node, position ){ return position; }, // transform a given node position. Useful for changing flow direction in discrete layouts 
-	              	  }
-                  //layout:{defaults}
-            		});
-		      		cy.center();
-		      	});
-
-		      }); */
-		    
-
-			function setFormat(ts){
-			  arrayTs = [];
-			  for(e in ts){arrayTs.push({x:Number(e),y:ts[e]})}
-			  return arrayTs;
-			}      
 });
+
+	function setFormat(ts){
+	  arrayTs = [];
+	  for(e in ts){arrayTs.push({x:Number(e),y:ts[e]})}
+	  return arrayTs;
+	}   
+
+    }); // fin document loaded
+  /*
+  $button.on('click', function(){
+  	$.getJSON("data/syntetic_control_600.json", function (data) {
+		var cy = cytoscape({
+  container: document.getElementById('cy'),
+  elements: data,
+  // so we can see the ids
+  style: 
+  [
+    {
+      selector: 'node',
+      style:{
+            //'label': 'data(label)',
+            //'width': '0.5px',
+            //'height': '0.5px',
+            'width': '0.015em',
+            'height': '0.015em',
+            'color': 'yellow',
+            'background-fit': 'contain',
+            'background-clip': 'none',
+            'border-style': 'solid',
+            'border-width': '0.0002em',
+            'border-color': 'white',
+            //'font-size' : '0.5px',
+            'font-size' : '0.02em',
+            'background-color': 'data(class)'
+      		}
+    },
+    {
+      selector: 'edge',
+      style: {
+            'text-background-color': 'yellow',
+            'text-background-opacity': 0.4,
+            //'width': '0.05em',
+            'width': '0.0015em',
+            'control-point-step-size': '0.3em'
+             }
+    }
+  ],
+
+  layout: {
+          name: 'preset',
+          positions: undefined, // map of (node id) => (position obj); or function(node){ return somPos; }
+          zoom: undefined, // the zoom level to set (prob want fit = false if set)
+          pan: undefined, // the pan level to set (prob want fit = false if set)
+          fit: true, // whether to fit to viewport
+          padding: 20, // padding on fit
+          animate: true, // whether to transition the node positions
+          animationDuration: 500, // duration of animation in ms if enabled
+          animationEasing: undefined, // easing of animation if enabled
+          animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+          ready: undefined, // callback on layoutready
+          stop: undefined, // callback on layoutstop
+          transform: function (node, position ){ return position; }, // transform a given node position. Useful for changing flow direction in discrete layouts 
+      	  }
+      //layout:{defaults}
+		});
+  		cy.center();
+  	});
+
+  }); */
+// fin del ajax
